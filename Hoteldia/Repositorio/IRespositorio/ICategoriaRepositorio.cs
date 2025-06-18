e1639c2603c1ff7dee80dc0c4e67ba69c6e5e133
Sergionoodle <73190422+Sergionoodle@users.noreply.github.com>
2025-06-19 01:51:12 +0200

    Upload Repository

---
+++ Hoteldia/Repositorio/IRespositorio/ICategoriaRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using Hoteldia.Modelos.DTO;
+
+namespace Hoteldia.Repositorio.IRespositorio
+{
+    public interface ICategoriaRepositorio
+    {
+        //Indicamos todas las categorias que vamos a obtener
+        public Task<IEnumerable<CategoriaDTO>> GetAllCategorias();
+
+        //Devolvemos una unica categoria donde le pasamos el ID
+        public Task<CategoriaDTO> GetCategoria(int categoriaId);
+
+        //Creamos una categoria
+        public Task<CategoriaDTO> CrearCategoria(CategoriaDTO categoriaDTO);
+
+        public Task<CategoriaDTO> ActualizarCategoria(int categoriaId, CategoriaDTO categoriaDTO);
+
+        public Task<CategoriaDTO> CategoriaExists(string nombre);
+
+        public Task<int> BorrarCategoria(int categoriaId);
+        //public Task<IEnumerable<CategoriaDTO>> GetDropDownCategorias();
+    }
+}
